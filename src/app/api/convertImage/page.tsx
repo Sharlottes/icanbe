ae639159e0f3b82a7ca15e0217d82b2770c05ee9
Sharlotte <aaa9810321@gmail.com>
2023-07-08 06:01:33 +0900

    aaaaaaaaaaaaaaaaa

---
+++ src/app/api/convertImage/page.tsx
@@ -0,0 +1,3 @@
+export default function A() {
+  return <p>no it's not a page. get out</p>;
+}
---
+++ src/app/convert/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import SpeechCollector from "@/components/SpeechCollector";
+import VideoRecorder from "@/core/VideoRecorder";
+import { useRef, useEffect, useState } from "react";
+import Webcam from "react-webcam";
+
+export default function ConvertPage() {
+  const [prompt, setPrompt] = useState("Stormtroopers in the Starwars");
+  const [resultURL, setResultURL] = useState("");
+  const videoRecorder = useRef<VideoRecorder>(new VideoRecorder("webcam_video"));
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  useEffect(() => {
+    videoRecorder.current.startVideo();
+  }, []);
+
+  const handleClick = async () => {
+    setIsProcessing(true);
+    const url = videoRecorder.current.captureVideo();
+    const { output } = await fetch(`/api/convertImage`, {
+      method: "POST",
+      body: JSON.stringify({
+        url,
+        prompt,
+      }),
+    }).then<{ output: string }>((res) => res.json());
+    setResultURL(output);
+    setIsProcessing(false);
+  };
+
+  return (
+    <main>
+      <div
+        style={{
+          display: "flex",
+          maxHeight: "600px",
+        }}
+      >
+        <Webcam id="webcam_video" audio />
+        <img src={resultURL} alt="" />
+      </div>
+      <div>
+        <button
+          style={{
+            display: "inline-block",
+          }}
+          disabled={isProcessing}
+          onClick={handleClick}
+        >
+          click me to capture!
+        </button>{" "}
+        {isProcessing && <span className="spinner" />}
+      </div>
+      <div>
+        <label>Prompt: </label>
+        <textarea value={prompt} onChange={(e) => setPrompt(e.target.value)} style={{ width: "100%" }} />
+      </div>
+      <SpeechCollector transcripts={prompt} setTranscripts={setPrompt} />
+    </main>
+  );
+}
